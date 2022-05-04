b10314fb340d3d14fd8a981b2559287e2da9baf5
Andrew <TroitskiyA@yandex.ru>
2022-05-04 20:14:13 +0500

    fix

--- src/middleWares/isAuth.ts
+++ src/middleWares/isAuth.ts
@@ -3,17 +3,17 @@ import { checkToken } from "../services/token.service";
 
 const isAuth: RequestHandler = (req: Request, res: Response, next: NextFunction) => {
   return next();
-  if (req.path = '/auth') {
-    return next();
-  }
-  const authHeader = req.header('Authorization');
-  if (authHeader) {
-    const [type, token] = authHeader.split(' ');
-    if (type === 'Bearer' && checkToken(token)) {
-      return next();
-    }
-  }
-  res.status(403).send('Wrong Authorization')
+  // if (req.path = '/auth') {
+  //   return next();
+  // }
+  // const authHeader = req.header('Authorization');
+  // if (authHeader) {
+  //   const [type, token] = authHeader.split(' ');
+  //   if (type === 'Bearer' && checkToken(token)) {
+  //     return next();
+  //   }
+  // }
+  // res.status(403).send('Wrong Authorization')
 }
 
 export default isAuth;
