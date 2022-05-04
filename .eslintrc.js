f40cad9bf5ba8d11ef960cdf9dcc1f11536731d9
Andrew <TroitskiyA@yandex.ru>
2022-05-04 13:59:33 +0500

    init: start project

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'eol-last': 0,
+    'linebreak-style': 0,
+    'no-console': 0,
+  },
+};
