7e525deb51c8fa62abfcf9db38dac8d48884f4c0
Andrew <TroitskiyA@yandex.ru>
2022-05-06 16:45:01 +0500

    test: add log

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -6,6 +6,7 @@ import { io } from './server.service';
 export const createUser = async (params: any) => {
   const newUser = new user(params);
   await newUser.save();
+  console.log(newUser);
   io.emit('login', newUser);
   return newUser;
 }
